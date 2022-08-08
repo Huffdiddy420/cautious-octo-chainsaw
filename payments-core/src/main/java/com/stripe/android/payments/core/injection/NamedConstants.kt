3b1c2477e3ddf10973e8c69da88b3c1d1d97c1cc
Huffdiddy420 <110040016+Huffdiddy420@users.noreply.github.com>
2022-08-07 22:06:05 -0500

    Initialize cfvNoError

---
+++ payments-core/src/main/java/com/stripe/android/payments/core/injection/NamedConstants.kt
@@ -0,0 +1,30 @@
+package com.stripe.android.payments.core.injection
+
+import com.stripe.android.model.PaymentIntent
+import com.stripe.android.model.SetupIntent
+import com.stripe.android.model.StripeIntent
+
+/**
+ * Name for injected set if strings to represent product usage for analytics.
+ */
+const val PRODUCT_USAGE = "productUsage"
+
+/**
+ * Name for user's publishable key
+ */
+const val PUBLISHABLE_KEY = "publishableKey"
+
+/**
+ * Name for user's account id
+ */
+const val STRIPE_ACCOUNT_ID = "stripeAccountId"
+
+/**
+ * Name to indicate whether the current [StripeIntent] is a [PaymentIntent] or [SetupIntent].
+ */
+const val IS_PAYMENT_INTENT = "isPaymentIntent"
+
+/**
+ * Name to indicate whether the current app is an instant app.
+ */
+const val IS_INSTANT_APP = "isInstantApp"
