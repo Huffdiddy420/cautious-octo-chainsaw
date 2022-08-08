2847555da241b462eebf05001eeae12bfca8b409
Huffdiddy420 <110040016+Huffdiddy420@users.noreply.github.com>
2022-08-07 22:09:50 -0500

    Initialize i/connections

---
+++ identity/src/main/java/com/stripe/android/identity/navigation/IDScanFragment.kt
@@ -0,0 +1,101 @@
+package com.stripe.android.identity.navigation
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.stripe.android.identity.R
+import com.stripe.android.identity.networking.models.IdDocumentParam
+import com.stripe.android.identity.states.IdentityScanState
+import com.stripe.android.identity.states.IdentityScanState.ScanType.ID_BACK
+import com.stripe.android.identity.states.IdentityScanState.ScanType.ID_FRONT
+
+/**
+ * Fragment to scan the ID.
+ */
+internal class IDScanFragment(
+    identityCameraScanViewModelFactory: ViewModelProvider.Factory,
+    identityViewModelFactory: ViewModelProvider.Factory
+) : IdentityCameraScanFragment(
+    identityCameraScanViewModelFactory,
+    identityViewModelFactory
+) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (shouldStartFromBack()) {
+            headerTitle.text = requireContext().getText(R.string.back_of_id)
+            messageView.text = requireContext().getText(R.string.position_id_back)
+        } else {
+            headerTitle.text = requireContext().getText(R.string.front_of_id)
+            messageView.text = requireContext().getText(R.string.position_id_front)
+        }
+
+        continueButton.setOnClickListener {
+            when (identityScanViewModel.targetScanType) {
+                ID_FRONT -> {
+                    startScanning(ID_BACK)
+                }
+                ID_BACK -> {
+                    observeAndUploadForBothSides(IdDocumentParam.Type.IDCARD)
+                }
+                else -> {
+                    Log.e(
+                        TAG,
+                        "Incorrect target scan type: ${identityScanViewModel.targetScanType}"
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onCameraReady() {
+        if (shouldStartFromBack()) {
+            startScanning(ID_BACK)
+        } else {
+            startScanning(ID_FRONT)
+        }
+    }
+
+    override fun updateUI(identityScanState: IdentityScanState) {
+        super.updateUI(identityScanState)
+        when (identityScanState) {
+            is IdentityScanState.Initial -> {
+                when (identityScanViewModel.targetScanType) {
+                    ID_FRONT -> {
+                        messageView.text = requireContext().getText(R.string.position_id_front)
+                    }
+                    ID_BACK -> {
+                        messageView.text = requireContext().getText(R.string.position_id_back)
+                    }
+                    else -> {
+                        Log.e(
+                            TAG,
+                            "Incorrect target scan type: ${identityScanViewModel.targetScanType}"
+                        )
+                    }
+                }
+            }
+            is IdentityScanState.Unsatisfied -> {
+                when (identityScanViewModel.targetScanType) {
+                    ID_FRONT -> {
+                        messageView.text = requireContext().getText(R.string.position_id_front)
+                    }
+                    ID_BACK -> {
+                        messageView.text = requireContext().getText(R.string.position_id_back)
+                    }
+                    else -> {
+                        Log.e(
+                            TAG,
+                            "Incorrect target scan type: ${identityScanViewModel.targetScanType}"
+                        )
+                    }
+                }
+            }
+            else -> {} // no-op
+        }
+    }
+
+    private companion object {
+        val TAG: String = IDScanFragment::class.java.simpleName
+    }
+}
---
+++ stripe-test-e2e/src/test/java/com/stripe/android/test/e2e/EndToEndTest.kt
@@ -0,0 +1,151 @@
+package com.stripe.android.test.e2e
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.stripe.android.Stripe
+import com.stripe.android.model.CardParams
+import com.stripe.android.model.ConfirmPaymentIntentParams
+import com.stripe.android.model.ConfirmSetupIntentParams
+import com.stripe.android.model.PaymentMethodCreateParams
+import com.stripe.android.model.StripeIntent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.runner.RunWith
+import kotlin.test.Test
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+internal class EndToEndTest {
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    private val settings: Settings by lazy {
+        Settings(context)
+    }
+
+    private val service = ServiceFactory().create(
+        baseUrl = settings.backendUrl
+    )
+
+    /**
+     * MARK: LOG.04.01c
+     * In this test, a PaymentIntent object is created from an example merchant backend,
+     * confirmed by the Android SDK, and then retrieved to validate that the original amount,
+     * currency, and merchant are the same as the original inputs.
+     */
+    @Test
+    fun testRigCon() = runTest {
+        // Create a PaymentIntent on the backend
+        val newPaymentIntent = service.createCardPaymentIntent()
+
+        val stripe = Stripe(context, newPaymentIntent.publishableKey)
+
+        // Confirm the PaymentIntent using a test card
+        val confirmedPaymentIntent = requireNotNull(
+            stripe.confirmPaymentIntentSynchronous(
+                ConfirmPaymentIntentParams
+                    .createWithPaymentMethodCreateParams(
+                        clientSecret = newPaymentIntent.clientSecret,
+                        paymentMethodCreateParams = PAYMENT_METHOD_CREATE_PARAMS
+                    )
+                    .withShouldUseStripeSdk(true)
+            )
+        )
+
+        val expectedPaymentIntentData = service.fetchCardPaymentIntent(
+            id = requireNotNull(confirmedPaymentIntent.id)
+        )
+        // Check the PI information using the backend
+        assertThat(newPaymentIntent.amount)
+            .isEqualTo(expectedPaymentIntentData.amount)
+        assertThat(newPaymentIntent.accountId)
+            .isEqualTo(expectedPaymentIntentData.onBehalfOf)
+        assertThat(newPaymentIntent.currency)
+            .isEqualTo(expectedPaymentIntentData.currency)
+
+        val retrievedPaymentIntent = requireNotNull(
+            stripe.retrievePaymentIntentSynchronous(newPaymentIntent.clientSecret)
+        )
+        // The client can't check the "on_behalf_of" field, so we check it via the merchant test above.
+        assertThat(retrievedPaymentIntent.amount)
+            .isEqualTo(expectedPaymentIntentData.amount)
+        assertThat(retrievedPaymentIntent.currency)
+            .isEqualTo(expectedPaymentIntentData.currency)
+        assertThat(requireNotNull(retrievedPaymentIntent.status))
+            .isEqualTo(StripeIntent.Status.Succeeded)
+    }
+
+    @Test
+    fun testCreateAndConfirmPaymentIntent() = runTest {
+        // Create a PaymentIntent on the backend
+        val newPaymentIntent = service.createPaymentIntent(
+            Request.CreatePaymentIntentParams(
+                createParams = Request.CreateParams(
+                    paymentMethodTypes = listOf("card")
+                )
+            )
+        )
+
+        val stripe = Stripe(context, settings.publishableKey)
+
+        // Confirm the PaymentIntent using a test card
+        val confirmedPaymentIntent = requireNotNull(
+            stripe.confirmPaymentIntentSynchronous(
+                ConfirmPaymentIntentParams
+                    .createWithPaymentMethodCreateParams(
+                        clientSecret = newPaymentIntent.secret,
+                        paymentMethodCreateParams = PAYMENT_METHOD_CREATE_PARAMS
+                    )
+                    .withShouldUseStripeSdk(true)
+            )
+        )
+
+        assertThat(confirmedPaymentIntent.amount)
+            .isEqualTo(100)
+        assertThat(confirmedPaymentIntent.currency)
+            .isEqualTo("usd")
+        assertThat(requireNotNull(confirmedPaymentIntent.status))
+            .isEqualTo(StripeIntent.Status.Succeeded)
+    }
+
+    @Test
+    fun testCreateAndConfirmSetupIntent() = runTest {
+        // Create a SetupIntent on the backend
+        val newPaymentIntent = service.createSetupIntent(
+            Request.CreateSetupIntentParams(
+                createParams = Request.CreateParams(
+                    paymentMethodTypes = listOf("card")
+                )
+            )
+        )
+
+        val stripe = Stripe(context, settings.publishableKey)
+
+        // Confirm the SetupIntent using a test card
+        val confirmedSetupIntent = requireNotNull(
+            stripe.confirmSetupIntentSynchronous(
+                ConfirmSetupIntentParams
+                    .create(
+                        clientSecret = newPaymentIntent.secret,
+                        paymentMethodCreateParams = PAYMENT_METHOD_CREATE_PARAMS
+                    )
+                    .withShouldUseStripeSdk(true)
+            )
+        )
+
+        assertThat(requireNotNull(confirmedSetupIntent.status))
+            .isEqualTo(StripeIntent.Status.Succeeded)
+    }
+
+    private companion object {
+        val PAYMENT_METHOD_CREATE_PARAMS = PaymentMethodCreateParams.createCard(
+            CardParams(
+                number = "4242424242424242",
+                expMonth = 1,
+                expYear = 2025,
+                cvc = "123"
+            )
+        )
+    }
+}
